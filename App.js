8edb29843a15d11ca88c7059fa0310f6a0e6413f
dinakaranuser <dinakarandev07@gmail.com>
2024-03-23 12:14:13 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import { View ,Text, SafeAreaView} from "react-native"
+
+const App = () => {
+    return (
+        <SafeAreaView>
+            <View>
+                <Text>Hellow World</Text>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+export default App
